bf3d48c250fbd83f0ec523178bef650dc90cb735
st168991 <st168991@stud.informatik.uni-stuttgart.de>
2020-02-11 18:28:56 +0100

    Added bash script for theo videos

---
+++ theoDownloader.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in {01..15};
+do 
+    wget https://fmi.uni-stuttgart.de/files/ti/teaching/w19/eti1/public/t$i.mp4
+    wget https://fmi.uni-stuttgart.de/files/ti/teaching/w19/eti1/public/b$i.mp4
+done
